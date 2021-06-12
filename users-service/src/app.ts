ec73a58b0adc38cfb274a8fc840f9f6aa0d7b32c
Abhi Thube <abhimanyuthube@gmail.com>
2021-06-11 21:12:31 -0700

    Configured eslint and prettier

--- users-service/src/app.ts
+++ users-service/src/app.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
 import express from 'express';
 
-export const app = express();
+const app = express();
 
 app.get('/', (_, res) => {
   res.status(200).json({ message: 'Hello world' });
 });
+
+export default app;
--- users-service/src/index.ts
+++ users-service/src/index.ts
@@ -1,4 +1,4 @@
-import { app } from './app';
+import app from './app';
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
