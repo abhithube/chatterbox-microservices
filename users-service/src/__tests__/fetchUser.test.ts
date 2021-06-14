3a26213bb46594dd5c8a5b17b4be66e272b14240
Abhi Thube <abhimanyuthube@gmail.com>
2021-06-13 18:17:00 -0700

    Fixed intentional typo in integration test

--- users-service/src/__tests__/fetchUser.test.ts
+++ users-service/src/__tests__/fetchUser.test.ts
@@ -20,7 +20,7 @@ test('should fetch an existing user', async () => {
   const res = await request(app).get('/api/users/test');
 
   expect(res.statusCode).toBe(200);
-  expect(res.body).toHaveProperty('username', 'tes');
+  expect(res.body).toHaveProperty('username', 'test');
 });
 
 test('should 404 if user does not exist', async () => {
