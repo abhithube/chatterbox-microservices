66d4f2d16ef29bbf5850970a25eee2d028e14e29
Abhi Thube <abhimanyuthube@gmail.com>
2021-06-22 03:03:09 -0700

    Renamed services based on API changes

--- profiles-service/src/config/kafka.ts
+++ profiles-service/src/config/kafka.ts
@@ -3,7 +3,7 @@ import { Kafka } from 'kafkajs';
 const brokers = [process.env.KAFKA_BROKERS || 'localhost:9093'];
 
 const kafka = new Kafka({
-  clientId: 'users-service',
+  clientId: 'profiles-service',
   brokers,
 });
 
