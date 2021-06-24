a33da18349e491eb91e5e3f5bf4269ad4b2b881e
Abhi Thube <abhimanyuthube@gmail.com>
2021-06-24 01:21:04 -0700

    Added support for connections and rooms in Socket.io

--- messages-service/src/app.ts
+++ messages-service/src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 import initializeTopics from './config/initializeTopics';
+import prisma from './config/prisma';
 import errorHandler from './middleware/errorHandler';
 import partyRoutes from './routes/partyRoutes';
 import topicRoutes from './routes/topicRoutes';
@@ -9,6 +12,63 @@ initializeTopics();
 consumeEvents();
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket: Socket) => {
+  socket.on('JOIN_REQUEST', async ({ userId, topicId }) => {
+    const user = await prisma.user.findUnique({ where: { publicId: userId } });
+    if (!user) {
+      socket.emit('ERROR', { status: 404, message: 'User not found' });
+      return;
+    }
+
+    const topic = await prisma.topic.findUnique({ where: { id: topicId } });
+    if (!topic) {
+      socket.emit('ERROR', { status: 404, message: 'Topic not found' });
+      return;
+    }
+
+    socket.join(topic.id.toString());
+  });
+
+  socket.on('LEAVE_REQUEST', async ({ userId, topicId }) => {
+    const user = await prisma.user.findUnique({ where: { publicId: userId } });
+    if (!user) {
+      socket.emit('ERROR', { status: 404, message: 'User not found' });
+      return;
+    }
+
+    const topic = await prisma.topic.findUnique({ where: { id: topicId } });
+    if (!topic) {
+      socket.emit('ERROR', { status: 404, message: 'Topic not found' });
+      return;
+    }
+
+    socket.leave(topic.id.toString());
+  });
+
+  socket.on('MESSAGE_SENT', async payload => {
+    const { body, userId, topicId } = payload;
+
+    const user = await prisma.user.findUnique({ where: { publicId: userId } });
+    if (!user) {
+      socket.emit('ERROR', { status: 404, message: 'User not found' });
+      return;
+    }
+
+    const topic = await prisma.topic.findUnique({ where: { id: topicId } });
+    if (!topic) {
+      socket.emit('ERROR', { status: 404, message: 'Topic not found' });
+      return;
+    }
+
+    const message = await prisma.message.create({
+      data: { body, userId: user.id, topicId },
+    });
+    io.in(topic.id.toString()).emit('MESSAGE_RECEIVED', message);
+  });
+});
 
 app.use(express.json());
 
@@ -16,4 +76,4 @@ app.use('/api', partyRoutes, topicRoutes);
 
 app.use(errorHandler);
 
-export default app;
+export default httpServer;
--- messages-service/src/index.ts
+++ messages-service/src/index.ts
@@ -1,4 +1,4 @@
-import app from './app';
+import httpServer from './app';
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
