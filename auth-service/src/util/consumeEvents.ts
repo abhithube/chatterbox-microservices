ee146c1448eab81f0f7f7ce2f4de7a7d7c48ab21
Abhi Thube <abhimanyuthube@gmail.com>
2021-07-02 22:12:11 -0700

    Updated user registration to send email verification link

--- auth-service/src/util/consumeEvents.ts
+++ auth-service/src/util/consumeEvents.ts
@@ -1,6 +1,8 @@
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 import consumer from '../config/consumer';
 import prisma from '../config/prisma';
+import emailUtil from './emailUtil';
 
 const consumeEvents = async (): Promise<void> => {
   await consumer.connect();
@@ -12,18 +14,37 @@ const consumeEvents = async (): Promise<void> => {
 
       switch (event.type) {
         case 'USER_CREATED':
-          await prisma.user.create({
-            data: {
-              sub: event.data.id,
-              username: event.data.username,
-              email: event.data.email,
-              avatarUrl: event.data.avatarUrl,
-              password: event.data.password
-                ? bcrypt.hashSync(event.data.password, 10)
-                : null,
-              verified: !event.data.password,
-            },
-          });
+          {
+            const isSocialAccount = !event.data.password;
+
+            if (isSocialAccount) {
+              await prisma.user.create({
+                data: {
+                  sub: event.data.id,
+                  username: event.data.username,
+                  email: event.data.email,
+                  avatarUrl: event.data.avatarUrl,
+                  verified: true,
+                },
+              });
+            } else {
+              const user = await prisma.user.create({
+                data: {
+                  sub: event.data.id,
+                  username: event.data.username,
+                  email: event.data.email,
+                  avatarUrl: event.data.avatarUrl,
+                  password: bcrypt.hashSync(event.data.password, 10),
+                  verificationToken: crypto.randomBytes(16).toString('hex'),
+                },
+              });
+
+              await emailUtil.sendVerificationEmail(
+                user.email,
+                user.verificationToken as string
+              );
+            }
+          }
           break;
         case 'USER_DELETED':
           await prisma.user.delete({
