031c054c712be89b8c6c0601e4231add93b07e3b
Abhi Thube <abhimanyuthube@gmail.com>
2021-07-16 04:25:49 -0700

    Moved Prisma schema file around

--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -23,7 +23,7 @@ import { LocalStrategy } from './strategies/local.strategy';
         host: configService.get('REDIS_HOST'),
         port: configService.get('REDIS_PORT'),
         password: configService.get('REDIS_PASSWORD'),
-        tls: true,
+        tls: {},
       }),
       inject: [ConfigService],
     }),
