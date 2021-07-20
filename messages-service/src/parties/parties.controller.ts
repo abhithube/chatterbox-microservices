8aeb81c687aa2ae87c50737ecfe19b041c6b582b
Abhi Thube <abhimanyuthube@gmail.com>
2021-07-20 14:50:37 -0700

    Merged topics module into parties module

--- messages-service/src/parties/parties.controller.ts
+++ messages-service/src/parties/parties.controller.ts
@@ -7,13 +7,19 @@ import {
   HttpStatus,
   Param,
   Post,
+  Query,
   Req,
   UseGuards,
 } from '@nestjs/common';
 import { Party } from '@prisma/client';
+import { MessageDto } from 'src/messages/dto/message.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RequestWithUser } from '../auth/interfaces/request-with-user.interface';
 import { CreatePartyDto } from './dto/create-party.dto';
+import { CreateTopicDto } from './dto/create-topic.dto';
+import { PartyWithUsersAndTopicsDto } from './dto/party-with-users-and-topics.dto';
+import { PartyDto } from './dto/party.dto';
+import { TopicDto } from './dto/topic.dto';
 import { PartiesService } from './parties.service';
 
 @Controller('parties')
@@ -21,13 +27,24 @@ export class PartiesController {
   constructor(private readonly partiesService: PartiesService) {}
 
   @Get()
-  async partiesHandler(): Promise<Party[]> {
-    return this.partiesService.getAllParties();
+  async partiesHandler(@Query('userId') userId: string): Promise<PartyDto[]> {
+    return this.partiesService.getAllParties(userId);
   }
 
   @Get(':id')
-  async partyHandler(@Param('id') id: string): Promise<Party> {
+  async partyHandler(
+    @Param('id') id: string,
+  ): Promise<PartyWithUsersAndTopicsDto> {
     return this.partiesService.getParty(id);
+  }
+
+  @Get(':id/topics/:topicId/messages')
+  async messagesHandler(
+    @Param('topicId') topicId: string,
+    @Query('limit') limit: number,
+    @Query('syncId') syncId: number,
+  ): Promise<MessageDto[]> {
+    return this.partiesService.getMessages(topicId, limit || 50, syncId || 1);
   }
 
   @UseGuards(JwtAuthGuard)
@@ -35,8 +52,18 @@ export class PartiesController {
   async createPartyHandler(
     @Req() req: RequestWithUser,
     @Body() createPartyDto: CreatePartyDto,
-  ): Promise<Party> {
+  ): Promise<PartyDto> {
     return this.partiesService.createParty(createPartyDto, req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id/topics')
+  async createTopicHandler(
+    @Req() req: RequestWithUser,
+    @Param('id') id: string,
+    @Body() createTopicDto: CreateTopicDto,
+  ): Promise<TopicDto> {
+    return this.partiesService.createTopic(createTopicDto, req.user.id, id);
   }
 
   @UseGuards(JwtAuthGuard)
@@ -64,7 +91,17 @@ export class PartiesController {
   async deletePartyHandler(
     @Req() req: RequestWithUser,
     @Param('id') id: string,
-  ): Promise<Party> {
+  ): Promise<PartyDto> {
     return this.partiesService.deleteParty(id, req.user.id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id/topics/:topicId')
+  async deleteTopicHandler(
+    @Req() req: RequestWithUser,
+    @Param('id') id: string,
+    @Param('topicId') topicId: string,
+  ): Promise<TopicDto> {
+    return this.partiesService.deleteTopic(topicId, req.user.id, id);
   }
 }
--- messages-service/src/topics/dto/create-topic.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateTopicDto {
-  name: string;
-  partyId: string;
-}
