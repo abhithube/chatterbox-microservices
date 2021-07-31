80766accf508e27b6c6622246e35f12619422302
Abhi Thube <abhimanyuthube@gmail.com>
2021-07-31 05:50:20 -0700

    Created email module using nodemailer

---
+++ common/mail/src/interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './mail-async-options.interface';
+export * from './mail-options.interface';
---
+++ common/mail/src/mail.service.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { createTransport, SendMailOptions, Transporter } from 'nodemailer';
+import { MailOptions } from './interfaces';
+
+@Injectable()
+export class MailService {
+  private transporter: Transporter;
+
+  constructor(@Inject('MAIL_OPTIONS') { transport, defaults }: MailOptions) {
+    this.transporter = createTransport(transport, defaults);
+  }
+
+  async sendMail(sendMailOptions: SendMailOptions): Promise<void> {
+    this.transporter.sendMail(sendMailOptions);
+  }
+}
