7511659b174fe0a9f25f4a828e5c44ca5b6ba825
Abhi Thube <athube@abhithube.com>
2022-11-12 14:34:47 -0800

    Initialized NestJS

---
+++ messages-service/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+})
+export class AppModule {}
