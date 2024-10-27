2f7a46db8539f5be9ae1e62f56856acda293e0d1
Abhi Thube <athube@abhithube.com>
2024-10-27 15:49:37 -0700

    Moved auth check to middleware

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,13 +1,7 @@
-import { auth, signIn } from '@/auth'
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default async function Home() {
-  const session = await auth()
-  if (!session) {
-    await signIn()
-  }
-
+export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -1,4 +1,17 @@
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const { auth: middleware } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig)
+
+export default auth(async function middleware(request: NextRequest) {
+  const session = await auth()
+
+  if (request.nextUrl.pathname.startsWith('/api/auth')) {
+    if (session) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  } else if (!session) {
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url))
+  }
+})
