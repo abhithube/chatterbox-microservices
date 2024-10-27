2af72017e038d58512ac580ac96006efe224c5dc
Abhi Thube <athube@abhithube.com>
2024-10-26 18:28:09 -0700

    Initialized SST

---
+++ next/sst.config.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: 'Chatterbox',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
+    }
+  },
+  async run() {
+    new sst.aws.Nextjs('ChatterboxNext')
+  },
+})
