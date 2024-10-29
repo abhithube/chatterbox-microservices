8e96e0c8507b6a274919ca9b5579937e83c9ecf1
Abhi Thube <athube@abhithube.com>
2024-10-29 03:15:44 -0700

    Updated sidebar to show topics of selected party

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -97,7 +97,7 @@ export default async function Page() {
 
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar partyId={party.id} topicId={TOPIC_ID} topics={party.topics} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
--- next/src/components/app-sidebar.tsx
+++ next/src/components/app-sidebar.tsx
@@ -13,168 +13,42 @@ import {
 } from '@/components/ui/sidebar'
 import * as React from 'react'
 import { PartySelector } from './party-selector'
-
-const data = {
-  versions: ['1.0.1', '1.1.0-alpha', '2.0.0-beta1'],
-  navMain: [
-    {
-      title: 'Getting Started',
-      url: '#',
-      items: [
-        {
-          title: 'Installation',
-          url: '#',
-        },
-        {
-          title: 'Project Structure',
-          url: '#',
-        },
-      ],
-    },
-    {
-      title: 'Building Your Application',
-      url: '#',
-      items: [
-        {
-          title: 'Routing',
-          url: '#',
-        },
-        {
-          title: 'Data Fetching',
-          url: '#',
-          isActive: true,
-        },
-        {
-          title: 'Rendering',
-          url: '#',
-        },
-        {
-          title: 'Caching',
-          url: '#',
-        },
-        {
-          title: 'Styling',
-          url: '#',
-        },
-        {
-          title: 'Optimizing',
-          url: '#',
-        },
-        {
-          title: 'Configuring',
-          url: '#',
-        },
-        {
-          title: 'Testing',
-          url: '#',
-        },
-        {
-          title: 'Authentication',
-          url: '#',
-        },
-        {
-          title: 'Deploying',
-          url: '#',
-        },
-        {
-          title: 'Upgrading',
-          url: '#',
-        },
-        {
-          title: 'Examples',
-          url: '#',
-        },
-      ],
-    },
-    {
-      title: 'API Reference',
-      url: '#',
-      items: [
-        {
-          title: 'Components',
-          url: '#',
-        },
-        {
-          title: 'File Conventions',
-          url: '#',
-        },
-        {
-          title: 'Functions',
-          url: '#',
-        },
-        {
-          title: 'next.config.js Options',
-          url: '#',
-        },
-        {
-          title: 'CLI',
-          url: '#',
-        },
-        {
-          title: 'Edge Runtime',
-          url: '#',
-        },
-      ],
-    },
-    {
-      title: 'Architecture',
-      url: '#',
-      items: [
-        {
-          title: 'Accessibility',
-          url: '#',
-        },
-        {
-          title: 'Fast Refresh',
-          url: '#',
-        },
-        {
-          title: 'Next.js Compiler',
-          url: '#',
-        },
-        {
-          title: 'Supported Browsers',
-          url: '#',
-        },
-        {
-          title: 'Turbopack',
-          url: '#',
-        },
-      ],
-    },
-  ],
-}
+import { Topic } from '@/lib/types'
+import Link from 'next/link'
 
 export async function AppSidebar({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
+  partyId,
+  topicId,
+  topics,
+}: {
+  partyId: string
+  topicId?: string
+  topics: Topic[]
+}) {
   const userId = (await auth())!.user!.id!
 
   return (
-    <Sidebar {...props}>
+    <Sidebar>
       <SidebarHeader>
-        <PartySelector
-          partyId="ec49e442-da01-4e22-8012-c4a0e0625a9a"
-          userId={userId}
-        />
+        <PartySelector partyId={partyId} userId={userId} />
       </SidebarHeader>
       <SidebarContent>
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
+        <SidebarGroup>
+          <SidebarGroupLabel>Topics</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {topics.map((topic) => (
+                <SidebarMenuItem key={topic.id}>
+                  <SidebarMenuButton asChild isActive={topic.id === topicId}>
+                    <Link href={`/topics/${partyId}/${topic.id}`}>
+                      {topic.title}
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
