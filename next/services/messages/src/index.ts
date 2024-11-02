20cb5a3e2168ef4d228d09c52fcc291396a57c75
Abhi Thube <athube@abhithube.com>
2024-11-01 21:20:49 -0700

    Use port from environment if specified

--- next/services/messages/src/index.ts
+++ next/services/messages/src/index.ts
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello world!')
 })
 
-const PORT = 8000
+const PORT = process.env.PORT ?? 8000
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
