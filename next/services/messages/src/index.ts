7aba4ff5145e119300bee6670e75389296a322d1
Abhi Thube <athube@abhithube.com>
2024-11-02 22:30:16 -0700

    Added configurable API path prefix

--- next/services/messages/src/index.ts
+++ next/services/messages/src/index.ts
@@ -2,9 +2,13 @@ import express from 'express'
 
 const app = express()
 
-app.get('/health', (req, res) => {
+const router = express.Router()
+
+router.get('/health', (req, res) => {
   res.send('OK')
 })
+
+app.use(process.env.API_PREFIX ?? '/api/v1', router)
 
 const PORT = process.env.PORT ?? 80
 
