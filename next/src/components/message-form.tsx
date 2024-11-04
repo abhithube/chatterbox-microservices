600e82bd7dc2adee1c093e6f5d525c475ca3f040
Abhi Thube <athube@abhithube.com>
2024-11-04 11:45:04 -0800

    Implemented message input submission

--- next/src/components/message-form.tsx
+++ next/src/components/message-form.tsx
@@ -1,14 +1,33 @@
+'use client'
+
 import { Input } from '@/components/ui/input'
+import { useSocket } from '@/lib/socket'
 import { Topic } from '@/lib/types'
 import { Play } from 'lucide-react'
+import { useState } from 'react'
 import { Button } from './ui/button'
 
-export async function MessageForm({ topic }: { topic: Topic }) {
+export function MessageForm({ topic }: { topic: Topic }) {
+  const { sendMessage } = useSocket()
+
+  const [message, setMessage] = useState('')
+
   return (
-    <form className="mx-4">
+    <form
+      className="mx-4"
+      onSubmit={(e) => {
+        e.preventDefault()
+        sendMessage(message)
+      }}
+    >
       <div className="flex space-x-4">
-        <Input id="message" placeholder={`Message '${topic.title}'`} />
-        <Button type="submit">
+        <Input
+          id="message"
+          placeholder={`Message '${topic.title}'`}
+          value={message}
+          onChange={(e) => setMessage(e.currentTarget.value)}
+        />
+        <Button type="submit" disabled={message.length === 0}>
           <Play />
         </Button>
       </div>
