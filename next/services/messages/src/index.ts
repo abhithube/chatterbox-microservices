301516d8ddbd973b3bd6183b3ecb1794a82124de
Abhi Thube <athube@abhithube.com>
2024-11-03 23:00:11 -0800

    Added Socket.io server

--- next/services/messages/src/index.ts
+++ next/services/messages/src/index.ts
@@ -1,6 +1,15 @@
 import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
+
+const API_PREFIX = process.env.API_PREFIX ?? '/api/v1'
+const PORT = process.env.PORT ?? 80
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  path: API_PREFIX + '/socket.io',
+})
 
 const router = express.Router()
 
@@ -8,10 +17,20 @@ router.get('/health', (req, res) => {
   res.send('OK')
 })
 
-app.use(process.env.API_PREFIX ?? '/api/v1', router)
+app.use(API_PREFIX, router)
 
-const PORT = process.env.PORT ?? 80
+io.on('connection', (socket) => {
+  const auth = socket.handshake.headers.authorization
+  if (!auth) {
+    socket.disconnect(true)
 
-app.listen(PORT, () => {
+    return
+  }
+
+  const token = auth.split(' ')[1]
+  console.log(token)
+})
+
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`)
 })
