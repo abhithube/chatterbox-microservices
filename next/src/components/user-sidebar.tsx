24ccab287310b9632649a2dd3a705a5ce97b64d6
Abhi Thube <athube@abhithube.com>
2024-11-05 03:40:40 -0800

    Added online status badge to user avatars in sidebar

--- next/src/components/user-sidebar.tsx
+++ next/src/components/user-sidebar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   Sidebar,
@@ -10,9 +12,13 @@ import {
   SidebarMenuItem,
   SidebarRail,
 } from '@/components/ui/sidebar'
+import { useSocket } from '@/lib/socket'
 import { Member } from '@/lib/types'
+import { cn } from '@/lib/utils'
 
 export function UserSidebar({ members }: { members: Member[] }) {
+  const { users } = useSocket()
+
   return (
     <Sidebar side="right">
       <SidebarContent>
@@ -23,15 +29,25 @@ export function UserSidebar({ members }: { members: Member[] }) {
               {members.map((member) => (
                 <SidebarMenuItem key={member.id}>
                   <SidebarMenuButton className="h-14">
-                    <Avatar>
-                      <AvatarImage src={member.image ?? undefined} />
-                      <AvatarFallback>
-                        {member.name
-                          .split(' ')
-                          .map((part) => part[0])
-                          .join('')}
-                      </AvatarFallback>
-                    </Avatar>
+                    <div className="relative">
+                      <Avatar>
+                        <AvatarImage src={member.image ?? undefined} />
+                        <AvatarFallback>
+                          {member.name
+                            .split(' ')
+                            .map((part) => part[0])
+                            .join('')}
+                        </AvatarFallback>
+                      </Avatar>
+                      <div
+                        className={cn(
+                          'absolute rounded-full w-4 h-4 -top-1 -right-1 border-2 border-sidebar',
+                          users.includes(member.id)
+                            ? 'bg-green-500'
+                            : 'bg-red-500',
+                        )}
+                      />
+                    </div>
                     {member.name}
                   </SidebarMenuButton>
                 </SidebarMenuItem>
